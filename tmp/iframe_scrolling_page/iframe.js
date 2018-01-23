af402b5bd1f7315c5c608d8a55fe6245b929d756
Pete Blois <blois@google.com>
2018-01-23 13:10:56 -0800

    Tue Jan 23 13:10:56 PST 2018

--- tmp/iframe_scrolling_page/iframe.js
+++ tmp/iframe_scrolling_page/iframe.js
@@ -1,23 +1,20 @@
 (async () => {
-  let count = 0;
+  let count = window.count || 0;
   for (let i = 0; i < 20; ++i) {
     const pre = document.createElement('pre');
     pre.textContent = `line ${count++}`;
     document.body.appendChild(pre);
   }
 
-  await new Promise(resolve => setTimeout(resolve, 100));
-  window.parent.postMessage({
-    height: document.documentElement.scrollHeight,
-  }, '*');
+  // document.body.onclick = function() {
+  //   for (let i = 0; i < 20; ++i) {
+  //     const pre = document.createElement('pre');
+  //     pre.textContent = `line ${count++}`;
+  //     document.body.appendChild(pre);
+  //   }
+  // };
 
-  document.body.onclick = function() {
-    for (let i = 0; i < 20; ++i) {
-      const pre = document.createElement('pre');
-      pre.textContent = `line ${count++}`;
-      document.body.appendChild(pre);
-    }
-  };
+  window.count = count;
   // await new Promise(resolve => setTimeout(resolve, 1000));
 
 })();
